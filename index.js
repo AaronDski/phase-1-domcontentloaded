981caf9ffcdd9536abab0129ae356f90706fc0d1
Aaron Dereszynski <aarondski123@gmail.com>
2021-07-05 15:10:04 -0600

    commit tree 38ae37c82e12287a45da277c8600033f7503226c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    let task = document.querySelector('#text');
+    task.textContent = 'This is really cool!';
+    
+  }); 
+  
+
+
+// let text = document.querySelector('#text')
+
+// let writing = 'This is really cool!'
